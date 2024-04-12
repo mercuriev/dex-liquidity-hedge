3325f3117aa996768d1036977098f5c381ddf5e0
Max <chepuhovia@gmail.com>
2024-04-12 18:50:13 +0700

    use single trigger() because parent class can't know current state (to do or undo)

--- src/App/Hedge/HedgeSell.php
+++ src/App/Hedge/HedgeSell.php
@@ -6,33 +6,31 @@ use Binance\Order\StopOrder;
 
 class HedgeSell extends Hedge
 {
-    protected function do(int $index): StopOrder
-    {
-        // +1 because lowest step doesn't have an open order
-        $amount = round($this->amount / (count($this) + 1), 5, PHP_ROUND_HALF_DOWN);
-        $order = new StopOrder();
-        $order->newOrderRespType = 'FULL';  // important for offline matching
-        $order->symbol = $this->symbol;
-        $order->side = 'SELL';
-        $order->quantity = $amount;
-        $order->setPrice($this->range[$index]);
-        $order->stopPrice = $order->price - $this->step;
-        return $order;
-    }
-
-    protected function undo(int $index): StopOrder|LimitOrder
+    protected function trigger(int $index): StopOrder|LimitOrder
     {
         $order = $this[$index];
-        $class = get_class($order);
-        $mirror = new $class;
-        $mirror->side = 'BUY';
-        $mirror->price = $order->price;
-        if ($order instanceof StopOrder) {
-            $mirror->newOrderRespType = 'FULL';  // important for offline matching
-            $mirror->stopPrice = $mirror->price;
-            $mirror->stopPrice += $this->step; // delay buyback (only if price is back up)
+
+        if (!$order || 'BUY' == $order->side) {
+            // +1 because lowest step doesn't have an open order
+            $amount = round($this->amount / (count($this) + 1), 5, PHP_ROUND_HALF_DOWN);
+            $new = new StopOrder();
+            $new->newOrderRespType = 'FULL';  // important for offline matching
+            $new->symbol = $this->symbol;
+            $new->side = 'SELL';
+            $new->quantity = $amount;
+            $new->setPrice($this->range[$index]);
+            #$new->stopPrice = $new->price - $this->step;
+        } else {
+            $new = new StopOrder();
+            $new->side = 'BUY';
+            $new->price = $order->price;
+            if ($new instanceof StopOrder) {
+                $new->newOrderRespType = 'FULL';  // important for offline matching
+                $new->stopPrice = $new->price;
+                $new->stopPrice += $this->step; // delay buyback (only if price is back up)
+            }
+            $new->quantity = $order->quantity;
         }
-        $mirror->quantity = $order->quantity;
-        return $mirror;
+        return $new;
     }
 }
