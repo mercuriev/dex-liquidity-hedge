62159e50c7d0a11f70d8109c1952f435ba72bcb7
Max Mercuriev <max@yogi.pw>
2024-04-16 02:23:54 +0700

    process Ctrl+C

--- hedge.php
+++ hedge.php
@@ -4,6 +4,13 @@ require 'vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\CommandLoader\ContainerCommandLoader;
 
+// call destructors on Ctrl+C
+pcntl_async_signals(true);
+pcntl_signal(SIGINT, function () {
+    echo "Shutting down...\n";
+    die;
+});
+
 $container = require 'config/container.php';
 
 $app = new Application();
