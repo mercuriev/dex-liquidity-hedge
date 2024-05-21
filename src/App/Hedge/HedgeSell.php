8db0685d29cf31226d7f66819a9febf0a7974ccb
Max Mercuriev <max@yogi.pw>
2024-05-22 04:51:16 +0800

    Revert "remove border orders creation"
    This reverts commit 3d9c33d7e33f34eb2cc0e419db1df0f9719d7694.

--- src/App/Hedge/HedgeSell.php
+++ src/App/Hedge/HedgeSell.php
@@ -58,6 +58,13 @@ class HedgeSell extends Hedge
             }
         }
 
+        // highest order
+        if ($index == 0 && 'BUY' == $this[$index]->side) {
+            $flip = $this->flip($this[$index]);
+            $this[$index] = $this->post($flip);
+            $this->log($index);
+        }
+
         $down = $index;
         while ($this->offsetExists(++$down)) {
             $next = $this[$down];
@@ -65,6 +72,13 @@ class HedgeSell extends Hedge
                 $this->new($down);
                 $this->log($down);
             }
+        }
+
+        // lowest order
+        if ($index == count($this) - 1 && 'SELL' == $this[$index]->side) {
+            $flip = $this->flip($this[$index]);
+            $this[$index] = $this->post($flip);
+            $this->log($index);
         }
 
         return null;
