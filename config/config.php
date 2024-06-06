43e7612e96abce66314389c2160926910eb44106
Max Mercuriev <max@yogi.pw>
2024-06-07 03:22:12 +0800

    move container factories to global config.php
    so that App/Config only has App related values for business logic

--- config/config.php
+++ config/config.php
@@ -1,10 +1,15 @@
 <?php
 
+use Amqp\Channel;
+use App\Logger\AmqpWriter;
 use Interop\Container\Containerinterface;
 use Laminas\ConfigAggregator\ArrayProvider;
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
+use Laminas\Log\Logger;
+use Laminas\Log\LoggerServiceFactory;
 use Laminas\ServiceManager\Factory\AbstractFactoryInterface;
+use Laminas\ServiceManager\Proxy\LazyServiceFactory;
 
 $aggregator = new ConfigAggregator([
     \Laminas\Log\ConfigProvider::class,
@@ -28,7 +33,30 @@ $aggregator = new ConfigAggregator([
                 },
 
                 // Enable container objects to be created by looking at construct parameters
-                \Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class
+                \Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory::class,
+            ],
+            'factories' => [
+                Logger::class => new class extends LoggerServiceFactory {
+                    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null) : Logger
+                    {
+                        $log = parent::__invoke($container, $requestedName, $options);
+                        $writer = $container->get(AmqpWriter::class);
+                        $writer->addFilter(Logger::NOTICE);
+                        $log->addWriter($writer);
+                        return $log;
+                    }
+                }
+            ],
+            'lazy_services' => [
+                'class_map' => [
+                    // lazy service solves circular dep with Logger
+                    Channel::class => Channel::class,
+                ],
+            ],
+            'delegators' => [
+                Channel::class => [
+                    LazyServiceFactory::class,
+                ],
             ],
         ]
     ]),
