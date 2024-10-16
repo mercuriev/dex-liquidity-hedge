26a7a9957a51609769968120663a5b5b08e46aed
Max Mercuriev <max@yogi.pw>
2024-10-16 21:51:58 +0500

    request position data only once on startup
    it's static

--- js/liquidity.js
+++ js/liquidity.js
@@ -10,44 +10,36 @@ const tokenId = 134574;
 (async () =>
 {
     // Initial fetch of pool address for given LP token so that we listen on this pool
-    const position = await PositionManager.positions(tokenId);
+    const LP = await PositionManager.positions(tokenId);
     const pool = await Pool.factory(await PoolFactory.getPool(
-        position[2],    // token0
-        position[3],    // token1
-        position[4]     // fee
-    ));
-    console.info(sprintf(
-        //'%s#%u : %.2f - %.2f : %.6f %s / %.2f %s',
-        '%s LP#%u : %.2f - %.2f',
-        pool.symbol, tokenId,
-        pool.tickToPrice(position[5]), pool.tickToPrice(position[6]),
+        LP[2],    // token0
+        LP[3],    // token1
+        LP[4]     // fee
     ));
 
     // Track position liquidity each time a swap occurs
     let lastPrice = null;
-    pool.onSwap((swap) => {
+    pool.onSwap((swap) =>
+    {
         const poolState = new V3Pool(pool.token0, pool.token1, pool.fee, swap.sqrtPriceX96, swap.liquidity, swap.tick);
-        const price = poolState.token0Price.toSignificant(6);
+        const price = poolState.token0Price.toFixed(2);
 
         if (lastPrice === price) return;
         lastPrice = price;
 
-        console.log(pool.symbol, 'price:', price);
-
-        PositionManager.positions(tokenId).then(res => {
-            const pos = new Position({
-                pool: poolState,
-                liquidity: String(res[7]),
-                tickLower: Number(res[5]),
-                tickUpper: Number(res[6])}
-            );
-            // 4 digits is the binance LOT_SIZE
-            console.log(sprintf(
-                'LP#%u : %.4f %s / %.2f %s',
-                tokenId,
-                pos.amount0.toFixed(4), pos.amount0.currency.symbol,
-                pos.amount1.toFixed(2), pos.amount1.currency.symbol
-            ));
-        });
+        const position = new Position({
+            pool: poolState,
+            liquidity: String(LP[7]),
+            tickLower: Number(LP[5]),
+            tickUpper: Number(LP[6])}
+        );
+        // 4 digits is the binance LOT_SIZE
+        console.log(sprintf(
+            '%s#%u (%.2f-%.2f) : %.2f : %.4f %s / %.2f %s',
+            pool.symbol, tokenId,
+            pool.tickToPrice(LP[5]), pool.tickToPrice(LP[6]), price,
+            position.amount0.toFixed(4), position.amount0.currency.symbol,
+            position.amount1.toFixed(2), position.amount1.currency.symbol
+        ));
     });
 })();
